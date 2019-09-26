1fe7946b5a735eb01b4555cdf96ef6cc326969f3
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 19:29:47 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan16/penugasan/PBO2PERT110118046Latihan16Penugasan.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pert1.pkg10118046.latihan16.penugasan;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program :
+ */
+public class PBO2PERT110118046Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c + "\n");
+        
+        c = a+b;
+        System.out.println("a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= a = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+
+}
